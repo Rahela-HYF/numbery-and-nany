8847575d8fe50dfbc4b4f6d27603f70735825bf6
Rahel <rahelsemukora@gmail.com>
2020-03-07 14:00:30 +0100

    step 4 adding handlers

---
+++ handlers/reset.js
@@ -0,0 +1,11 @@
+function resetHandler(){
+  //  alert("reset");
+    entries.numbery = [];
+    entries.nany =[];
+
+
+    document.getElementById('number').innerHTML = entries.numbery;
+    document.getElementById('nany').innerHTML = entries.nany;
+    document.getElementById('input').value ="";
+
+}
